a6ee2a5d02ba8e25a37190e08cabab360615c261
Lucas Rocha <lucas_r.rocha@outlook.com>
2021-03-13 00:17:15 -0300

    Created functions add tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -14,16 +14,31 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  useEffect(() => {
+    console.log(tasks)
+  }, [tasks])
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle === "") {
+      return;
+    }
+    setTasks([...tasks, {id: Math.floor(Math.random() * 100 + 1), title: newTaskTitle, isComplete: false}])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+  //  Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const selectedTask = tasks.filter(task => {
+      return task.id === id
+    })
+
+    selectedTask.forEach(task => task.isComplete = true)
+    setTasks([...tasks])
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((index) => index.id !== id))
   }
 
   return (
